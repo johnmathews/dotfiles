4d8bd6fef3bc33a91b6bc3fba41ba2e60b002cb5
John Mathews <johnmatthews4@wayfair.com>
2021-11-05 17:11:33 +0100

    2021-11-05

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -39,7 +39,7 @@
     time
     command_execution_time  # duration of the last command
     vi_mode                 # vi mode (you don't need this if you've enabled prompt_char)
-    virtualenv
+    # virtualenv
     # os_icon               # os identifier
     dir                     # current directory
     vcs                     # git status
@@ -59,7 +59,7 @@
     background_jobs         # presence of background jobs
     direnv                  # direnv status (https://direnv.net/)
     asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
-    # virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
+    # virtualenv            # python virtual environment (https://docs.python.org/3/library/venv.html)
     anaconda                # conda environment (https://conda.io/)
     pyenv                   # python environment (https://github.com/pyenv/pyenv)
     goenv                   # go environment (https://github.com/syndbg/goenv)
@@ -97,7 +97,7 @@
     # swap                  # used swap
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
-    # time                    # current time
+    # time                  # current time
     # =========================[ Line #2 ]=========================
     newline
     # ip                    # ip address and bandwidth usage for a specified network interface
