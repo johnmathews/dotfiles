4eb2b4dc8c997c67cfff8141f6e32da1cbe9fc6a
John Mathews <johnmatthews4@wayfair.com>
2021-11-30 22:42:03 +0100

    updates

--- dot_fzf.zsh
+++ dot_fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/jm943l/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/Users/jm943l/.fzf/bin"
+if [[ ! "$PATH" == */Users/$USER/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/Users/$USER/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/jm943l/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/Users/$USER/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/jm943l/.fzf/shell/key-bindings.zsh"
+source "/Users/$USER/.fzf/shell/key-bindings.zsh"
