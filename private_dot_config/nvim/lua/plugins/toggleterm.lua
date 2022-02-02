e83ff03460484464dc6c82fae2df602e51e3fca7
John Mathews <johnmatthews4@wayfair.com>
2022-02-02 13:10:02 +0100

    toggleterm

--- private_dot_config/nvim/lua/plugins/toggleterm.lua
+++ private_dot_config/nvim/lua/plugins/toggleterm.lua
@@ -26,15 +26,16 @@ toggleterm.setup{
   end,
   -- open_mapping = [[<c-\>]],
   open_mapping = [[<C-I>t]],
+  insert_mappings = false, -- whether or not the open mapping applies in insert mode
+  terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+  start_in_insert = false,
+
   -- on_open = fun(t: Terminal), -- function to run when the terminal opens
   -- on_close = fun(t: Terminal), -- function to run when the terminal closes
   hide_numbers = true, -- hide the number column in toggleterm buffers
   shade_filetypes = {},
   shade_terminals = true,
   shading_factor = '1', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
-  start_in_insert = true,
-  insert_mappings = true, -- whether or not the open mapping applies in insert mode
-  terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
   persist_size = true,
   -- direction = 'vertical' | 'horizontal' | 'window' | 'float',
   direction = 'float',
