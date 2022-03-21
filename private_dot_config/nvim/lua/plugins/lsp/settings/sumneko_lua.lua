3639fcfe15feb6fd6c9681325606411365d904d7
John Mathews <johnmatthews4@wayfair.com>
2022-03-21 11:44:39 +0100

    lsp sumnekolua disable white-space

--- private_dot_config/nvim/lua/plugins/lsp/settings/sumneko_lua.lua
+++ private_dot_config/nvim/lua/plugins/lsp/settings/sumneko_lua.lua
@@ -1,4 +1,6 @@
 return {
+  -- Schema: https://raw.githubusercontent.com/sumneko/vscode-lua/master/setting/schema.json
+  -- help: https://github.com/sumneko/lua-language-server/wiki/Setting#working-example 
   settings = {
     Lua = {
       diagnostics = {
@@ -6,6 +8,9 @@ return {
           "vim",
           "require",
         },
+        disable = {
+          "trailing-space",
+        }
       },
       workspace = {
         library = {
